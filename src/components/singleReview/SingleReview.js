dddc852990660a6b09bc73b8335fd58bce9955ae
UttamSaha <uttamsaha982@gmail.com>
2022-04-04 16:28:43 +0600

    rating added

--- src/components/singleReview/SingleReview.js
+++ src/components/singleReview/SingleReview.js
@@ -1,16 +1,25 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import './SingleReview.css'
 
 const SingleReview = (props) => {
     const {name, img, comment, ratings} = props.review;
     console.log(props);
+    const array = [];
+    for(let i = 1; i <= ratings; i++){
+        array.push(5);
+    }
     return (
-        <div className="review">
-            <p>{comment}</p>
-            <h4>{name}</h4>
-            <img src={img} alt="" />
-            <h5>{ratings}</h5>
-        </div>
-    )
+      <div className="review">
+        <p>{comment}</p>
+        <img src={img} alt="" /> <br />
+        <h4>{name}</h4>
+        <span>Ratings: </span>
+        {array.map((array) => (
+          <FontAwesomeIcon className="icons" icon={faStar}></FontAwesomeIcon>
+        ))}
+      </div>
+    );
 }
 export default SingleReview;
