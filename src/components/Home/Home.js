6bdd7037d743187db818a7dee70badae4a852e22
UttamSaha <uttamsaha982@gmail.com>
2022-04-04 18:35:08 +0600

    some decoration modifided

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
-import React from "react";  
+import React from "react";
 import useReviews from "../../CustomHooks/useReviews";
 import SingleReview from "../singleReview/SingleReview";
-import './Home.css'
+import "./Home.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowCircleRight } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
@@ -11,49 +11,50 @@ const Home = () => {
   const navigate = useNavigate();
 
   const seeAllReviews = () => {
-    navigate('/reviews')
-  }
-    return (
-      <div className="home-container">
-        <div className="home">
-          <div className="home-content">
-            <h1>
-              Find The Car Here, <br />
-              <span className="title">Build Your Deal</span>
-            </h1>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Molestiae quod quas modi quidem saepe nemo nostrum aut, fugit
-              soluta autem blanditiis ducimus, similique dignissimos
-              accusantium. Qui, blanditiis possimus. Aliquid, totam?
-            </p>
-            <button>
-              Live Demo{" "}
-              <FontAwesomeIcon className="fa-icon" icon={faArrowCircleRight}></FontAwesomeIcon>
-            </button>
-          </div>
-          <div>
-            <img src="car1.png" alt="" />
-          </div>
-        </div>
-        <div className="customer-review">
-          <h1 className="customer-review-title">Customer Reviews(3)</h1>
-          <div className="review-container">
-            <div className="reviews">
-              {reviews.map((review) => (
-                <SingleReview review={review} key={review.id}></SingleReview>
-              ))}
-            </div>
-          </div>
-          <button onClick={seeAllReviews} className="all-reviews-btn">
-            See All Reviews{" "}
+    navigate("/reviews");
+  };
+  return (
+    <div className="home-container">
+      <div className="home">
+        <div className="home-content">
+          <h1>
+            Welcome To Our,
+            <br />
+            <span className="title">Car Shop BD </span>
+          </h1>
+          <p className="home-details">
+            Car Shop BD is the best online car selling company. Here you will get all kinds of cars. You can also take car rent from here. Load service is available for buy cars. Please buy your favourite car from here. Thank you so much.
+          </p>
+          <button>
+            Live Demo{" "}
             <FontAwesomeIcon
               className="fa-icon"
               icon={faArrowCircleRight}
-            ></FontAwesomeIcon>{" "}
+            ></FontAwesomeIcon>
           </button>
         </div>
+        <div>
+          <img src="car1.png" alt="" />
+        </div>
       </div>
-    );
-}
+      <div className="customer-review">
+        <h1 className="customer-review-title">Customer Reviews(3)</h1>
+        <div className="review-container">
+          <div className="reviews">
+            {reviews.map((review) => (
+              <SingleReview review={review} key={review.id}></SingleReview>
+            ))}
+          </div>
+        </div>
+        <button onClick={seeAllReviews} className="all-reviews-btn">
+          See All Reviews{" "}
+          <FontAwesomeIcon
+            className="fa-icon"
+            icon={faArrowCircleRight}
+          ></FontAwesomeIcon>{" "}
+        </button>
+      </div>
+    </div>
+  );
+};
 export default Home;
